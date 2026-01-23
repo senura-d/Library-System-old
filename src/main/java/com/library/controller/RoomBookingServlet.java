6bdbb0f94dad74619b1681314e5ac9135561f293
himansanthusa <himansanthusa2004@gmail.com>
2026-01-23 14:14:18 +0530

    Interactive room booking system

--- src/main/java/com/library/controller/RoomBookingServlet.java
+++ src/main/java/com/library/controller/RoomBookingServlet.java
@@ -7,15 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/RoomServlet")
-public class RoomServlet extends HttpServlet {
+@WebServlet("/RoomBookingServlet")
+public class RoomBookingServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String room = request.getParameter("roomName");
+        String roomName = request.getParameter("roomName");
         String date = request.getParameter("date");
-        String time = request.getParameter("time");
 
-        // In a real app, save to DB here: INSERT INTO bookings...
-        System.out.println("Booking: " + room + " on " + date + " at " + time);
+        // In a real app, you would save this to the database here.
+        // For now, we simulate a successful booking.
+        System.out.println("Booking Confirmed: " + roomName + " for " + date);
 
         response.sendRedirect("member-dashboard?msg=RoomBooked");
     }
