1012a1e34b5ea5f53286e61033e6dc8c0e60e6ba
Eshan Nethmin <eshannethmin2004@gmail.com>
2026-01-23 12:42:07 +0530

    added borrowing,return,reminder functionality

--- src/main/java/com/library/dao/DBConnection.java
+++ src/main/java/com/library/dao/DBConnection.java
@@ -5,18 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+    // Matches your credentials: root / Askme458
     private static final String URL = "jdbc:mysql://localhost:3306/library_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "Askme458"; // <--- Did you put your real password here?
+    private static final String PASSWORD = "Askme458";
 
-    public static Connection getConnection() {
-        Connection con = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return con;
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        // Load MySQL Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
